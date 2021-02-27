38eb0c96c57f8d35998d33c11b9f2a9e466f16a1
sadfun <32015025+sadfun@users.noreply.github.com>
2021-02-27 11:50:08 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import pyxel
+
+pyxel.init(160, 120)
+
+def update():
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()
+
+def draw():
+    pyxel.cls(0)
+    pyxel.rect(10, 10, 20, 20, 11)
+
+pyxel.run(update, draw)
