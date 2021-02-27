1aca6f98a70bf07bdb86c4f78d77e2d320bffa6d
drboss06 <50492531+drboss06@users.noreply.github.com>
2021-02-27 12:35:49 +0700

    map

--- map.py
+++ map.py
@@ -6,10 +6,12 @@ mapa = [
     '...................',
     '.##........##......',
     '...................',
+    '...................',
+    '...................',
 ]
 
 inf_rect = []
-for i in mapa:
-    for k in i:
-        if k == '#':
-            inf_rect.append([i, k, st.WIDTH, st.HEIGTH])
+for i in range(len(mapa)):
+    for k in range(len(mapa[i])):
+        if mapa[i][k] == '#':
+            inf_rect.append([i + 20, k + 20, st.WIDTH, st.HEIGTH])
--- sasha.py
+++ sasha.py
@@ -1,19 +1,28 @@
 import pyxel
 from cRect import cRect
+from map import inf_rect
+
+GRAVITY = 1
+
 
 class App:
     def __init__(self):
         pyxel.init(160, 120)
+        self.width = 10
+        self.height = 10
         self.x_dest = 0
-        self.y_dest = 0
+        self.y_dest = 120 - self.height
         self.x = 0
-        self.y = 0
-        pyxel.run(self.update, self.draw)
+        self.y = self.y_dest
+
+        self.player = cRect(self.x, self.y, 10, 10, pyxel.COLOR_LIME)
+
+        pyxel.run(self.update, self.draw_level)
 
     def update(self):
-        if pyxel.btnp(pyxel.KEY_S) and self.y == self.y_dest:
+        if pyxel.btnp(pyxel.KEY_S):
             self.y_dest += 10
-        if pyxel.btnp(pyxel.KEY_W) and self.y == self.y_dest:
+        if pyxel.btnp(pyxel.KEY_W):
             self.y_dest -= 10
         if pyxel.btnp(pyxel.KEY_D) and self.x == self.x_dest:
             self.x_dest += 10
@@ -22,12 +31,29 @@ class App:
 
         if self.y_dest != self.y:
             self.y += 1 if self.y_dest > self.y else -1
+            if self.y < 0:
+                self.y = 0
+            if self.y > 120 - self.height:
+                self.y = 120 - self.height
         if self.x_dest != self.x:
             self.x += 1 if self.x_dest > self.x else -1
+            if self.x < 0:
+                self.x = 0
+            if self.x > 160 - self.width:
+                self.x = 160 - self.width
 
-    def draw(self):
+        if self.y_dest == self.y and self.y_dest != 120 - self.height: #and self.y >= 160 - self.height:
+            self.y_dest = 120 - self.height
+
+        self.player.set_pos(self.x, self.y)
+
+    def draw_player(self):
         pyxel.cls(pyxel.COLOR_LIGHTBLUE)
-        pyxel.rect(self.x, self.y, 10, 10, pyxel.COLOR_LIME)
+        self.player.draw()
+    
+    def draw_level(self):
+        for i in inf_rect:
+            pyxel.rect(i[0], i[1], i[2], i[3], 11)
         
 
 App()
