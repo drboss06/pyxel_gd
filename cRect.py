b893d6386fe52cf8fccda05c1e7ccbb7da804334
ArtiArtem8 <77927789+ArtiArtem8@users.noreply.github.com>
2021-02-27 12:41:30 +0700

    platform

--- cRect.py
+++ cRect.py
@@ -24,3 +24,19 @@ class cRect:
     def set_pos(self, x, y):
         self.x = x
         self.y = y
+
+
+class cPlatform(cRect):
+    def __init(self, x, y, w, h, col):
+        super().__init__(x, y, w, h, col)
+        self.active = True
+    
+    def checkcolls(self, player):
+        if self.collision(player) and self.active:
+            if player.falling:
+                self.active = False
+                return True
+    
+    def draw(self):
+        if self.active:
+            self.draw()
