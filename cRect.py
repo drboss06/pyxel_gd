8c9a2ababea4bb3a7a7fbc1f9437f6cfd6919046
ArtiArtem8 <77927789+ArtiArtem8@users.noreply.github.com>
2021-02-27 12:07:28 +0700

    Update cRect.py

--- cRect.py
+++ cRect.py
@@ -2,15 +2,25 @@ import pyxel
 
 
 class cRect:
-    def __init__(self, x, y, w, h):
+    def __init__(self, x, y, w, h, col=0):
         self.x, self.y = x, y
         self.w, self.h = w, h
+        self.vel = (0, 0)
+        self.col = col
     
     def draw(self):
-        pyxel.rect(self.x, self.y, self.w, self.h)
+        pyxel.rect(self.x, self.y, self.w, self.h, self.col)
     
     def collision(self, other):
         return self.x < other.x + other.w and \
                self.x + self.w > other.x and \
                self.y < other.y + other.h and \
                self.y + self.h > other.y
+    
+    def update(self):
+        self.x += self.vel[0]
+        self.y += self.vel[1]
+    
+    def set_pos(self, x, y):
+        self.x = x
+        self.y = y
